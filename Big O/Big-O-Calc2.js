2ee178cca814eee0b8f3931da3f1ea43b9636235
Vairamuthu <mvairamuthu2003@gmail.com>
2023-08-06 14:06:54 +0530

    updated my DSA notes with Big O Challenge

--- Big O/Big-O-Calc2.js
+++ Big O/Big-O-Calc2.js
@@ -1,15 +1,17 @@
 function anotherfunChallenge(input) {
-  let a = 5;
-  let b = 10;
-  let c = 50;
+  let a = 5; // O(1)
+  let b = 10; // O(1)
+  let c = 50; // O(1)
   for (let i = 0; i < input; i++) {
-    let x = i + 1;
-    let y = i + 2;
-    let z = i + 3;
+    let x = i + 1; // O(n)
+    let y = i + 2; // O(n)
+    let z = i + 3; // O(n)
   }
   for (let j = 0; j < input; j++) {
-    let p = j * 2;
-    let q = j * 2;
+    let p = j * 2; // O(n)
+    let q = j * 2; // O(n)
   }
-  let whoAmI = "I don't know";
+  let whoAmI = "I don't know"; // O(1)
 }
+
+// Totally O(4 + 5n)
