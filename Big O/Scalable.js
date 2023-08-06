09128de247adbf2be298c89ef47e5ba405904f9d
Vairamuthu <mvairamuthu2003@gmail.com>
2023-08-06 17:25:50 +0530

    added the space complexity

---
+++ Big O/Scalable.js
@@ -0,0 +1,16 @@
+const tweets = [{
+    msg:'hi',
+    date: 2012
+    },{
+    msg:'hello',
+    date: 2022
+    },{
+    msg:'awesome',
+    date: 2023
+    }];  
+
+const tweet = ['hi','hello','bonjour']
+console.log(tweet[0]); // O(1)
+console.log(tweet[tweet.length-1]); // O(1)
+
+'dsfsdfsfdsf'.length() // based on length it varies
