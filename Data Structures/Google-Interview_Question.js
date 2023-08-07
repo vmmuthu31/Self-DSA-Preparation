06898e5ec9a92962e191d73d70ee58597d69f5ec
Vairamuthu <mvairamuthu2003@gmail.com>
2023-08-07 22:56:46 +0530

    completed the problem

--- Data Structures/Google-Interview_Question.js
+++ Data Structures/Google-Interview_Question.js
@@ -10,3 +10,18 @@
 
 // 2 parameter - array inputs -- no size limit
 // return true/false
+
+const array1 = ["a", "b", "c", "x"];
+const array2 = ["z", "y", "j"];
+
+function containCommonItem(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+containCommonItem(array1, array2);
